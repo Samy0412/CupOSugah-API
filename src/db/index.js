11a4b71467659e07ea036699e95f831a99ee1a88
SAMANTHA GADET <samantha@merinio.com>
2022-12-29 15:19:21 +0100

    Add a DATABASE_URL in environment file

--- src/db/index.js
+++ src/db/index.js
@@ -1,13 +1,10 @@
 const pg = require("pg");
 
-const { PGHOST, PGDATABASE, PGUSER, PGPASSWORD, ENDPOINT_ID } = process.env;
-const URL = `postgres://${PGUSER}:${PGPASSWORD}@${PGHOST}/${PGDATABASE}?options=project%3D${ENDPOINT_ID}`;
-
 const client = new pg.Client({
-  connectionString: URL || "",
-  ssl: {
-    rejectUnauthorized: false,
-  },
+  connectionString: process.env.DATABASE_URL || "",
+  // ssl: {
+  //   rejectUnauthorized: false,
+  // },
 });
 
 client
